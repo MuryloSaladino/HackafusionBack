b0795b2da46b210cb09833c29933eee9753921e4
MuryloSaladino <murylosaladino@gmail.com>
2024-07-16 08:06:50 -0300

    feat: calendar event

---
+++ src/main/java/com/greenteam/schoolmanager/entities/CalendarEvent.java
@@ -0,0 +1,51 @@
+package com.greenteam.schoolmanager.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Table @Entity @Getter
+public class CalendarEvent extends BaseEntity {
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+
+    @ManyToOne @JoinColumn(nullable = false)
+    private StudentGangEntity studentGangEntity;
+
+    @ManyToOne @JoinColumn()
+    private UserEntity userEntity;
+
+
+    public void setTitle(String title) {
+        this.update();
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.update();
+        this.description = description;
+    }
+
+    public void setDate(Date date) {
+        this.update();
+        this.date = date;
+    }
+
+    public void setStudentGangEntity(StudentGangEntity studentGangEntity) {
+        this.update();
+        this.studentGangEntity = studentGangEntity;
+    }
+
+    public void setUserEntity(UserEntity userEntity) {
+        this.update();
+        this.userEntity = userEntity;
+    }
+}
