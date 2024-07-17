6b191d2549c12e93e4d9ddf0bb2e0ccdd1b23dbc
JoviOakz <joao.mateussdc@gmail.com>
2024-07-17 08:38:48 -0300

    eba

--- src/main/java/com/greenteam/schoolmanager/repositories/StudentGangDisciplineRepository.java
+++ src/main/java/com/greenteam/schoolmanager/repositories/StudentGangDisciplineRepository.java
@@ -1,8 +1,13 @@
 package com.greenteam.schoolmanager.repositories;
 
 import com.greenteam.schoolmanager.entities.StudentGangDisciplineEntity;
+import com.greenteam.schoolmanager.entities.StudentGangEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface StudentGangDisciplineRepository extends CrudRepository<StudentGangDisciplineEntity, Long> {}
+public interface StudentGangDisciplineRepository extends CrudRepository<StudentGangDisciplineEntity, Long> {
+    List<StudentGangDisciplineEntity> findByGang(StudentGangEntity studentGang);
+}
--- src/main/java/com/greenteam/schoolmanager/repositories/StudentGangRepository.java
+++ src/main/java/com/greenteam/schoolmanager/repositories/StudentGangRepository.java
@@ -1,8 +1,13 @@
 package com.greenteam.schoolmanager.repositories;
 
 import com.greenteam.schoolmanager.entities.StudentGangEntity;
+import com.greenteam.schoolmanager.enums.DisciplineType;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface StudentGangRepository extends CrudRepository<StudentGangEntity, Long> {}
+public interface StudentGangRepository extends CrudRepository<StudentGangEntity, Long> {
+    List<StudentGangEntity> findByMainDisciplineType(DisciplineType mainDisciplineType);
+}
