9345658551033b42893095257309ef82a17fd900
JoviOakz <joao.mateussdc@gmail.com>
2024-07-22 14:14:06 -0300

    feat: insomnia finalized

--- src/main/java/com/greenteam/schoolmanager/config/CorsConfiguration.java
+++ src/main/java/com/greenteam/schoolmanager/config/CorsConfiguration.java
@@ -11,7 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins("http://localhost:5257/")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE");
     }
 }
