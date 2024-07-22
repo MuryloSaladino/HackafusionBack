3e18a7c82e4b988284558d3147521535dd35b3c5
JoviOakz <joao.mateussdc@gmail.com>
2024-07-22 16:55:23 -0300

    fixed answer response

--- src/main/java/com/greenteam/schoolmanager/dto/answer/AnswerEntityResponse.java
+++ src/main/java/com/greenteam/schoolmanager/dto/answer/AnswerEntityResponse.java
@@ -1,6 +1,7 @@
 package com.greenteam.schoolmanager.dto.answer;
 
 import com.greenteam.schoolmanager.dto.question.QuestionEntityResponse;
+import com.greenteam.schoolmanager.dto.user.UserEntityResponse;
 import com.greenteam.schoolmanager.entities.AnswerEntity;
 
 public class AnswerEntityResponse {
@@ -8,10 +9,12 @@ public class AnswerEntityResponse {
     public Long id;
     public String desciption;
     public QuestionEntityResponse question;
+    public UserEntityResponse user;
 
     public AnswerEntityResponse(AnswerEntity answerEntity) {
         this.id = answerEntity.getId();
         this.desciption = answerEntity.getDescription();
         this.question = new QuestionEntityResponse(answerEntity.getQuestion());
+        this.user = new UserEntityResponse(answerEntity.getUser());
     }
 }
