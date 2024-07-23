eb1b3185bcabfaf1f0542094448d9a46f62d0f19
MuryloSaladino <murylosaladino@gmail.com>
2024-07-23 01:36:12 -0300

    fix: remove question from dto

--- src/main/java/com/greenteam/schoolmanager/dto/answer/AnswerEntityResponse.java
+++ src/main/java/com/greenteam/schoolmanager/dto/answer/AnswerEntityResponse.java
@@ -8,13 +8,11 @@ public class AnswerEntityResponse {
 
     public Long id;
     public String desciption;
-    public QuestionEntityResponse question;
     public UserEntityResponse user;
 
     public AnswerEntityResponse(AnswerEntity answerEntity) {
         this.id = answerEntity.getId();
         this.desciption = answerEntity.getDescription();
-        this.question = new QuestionEntityResponse(answerEntity.getQuestion());
         this.user = new UserEntityResponse(answerEntity.getUser());
     }
 }
